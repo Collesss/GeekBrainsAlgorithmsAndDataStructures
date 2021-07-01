38dae3d4d8b71ff59284f2b47a791a92b7399edc
Sergey Lesikov <sergey_lesikov@mail.ru>
2021-07-01 21:19:04 +0300

    update Project 3; mod. README.md

--- Lesson1Project3/Lesson1Project3.cs
+++ Lesson1Project3/Lesson1Project3.cs
@@ -9,19 +9,39 @@ namespace Lesson1Project3
             public int F { get; set; }
             public int Expected { get; set; }
             public Exception ExpectedException { get; set; }
+
+            public override string ToString() => $"F: {F}; Expected: {Expected};";
         }
 
         static void Test(TestCase testCase)
         {
+            int actual = 0;
 
+            try
+            {
+                actual = Fibonaci(testCase.F);
+
+                if (actual == testCase.Expected)
+                    Console.WriteLine($"VALID TEST actual: {actual}; {testCase}");
+                else
+                    Console.WriteLine($"INVALID TEST actual: {actual}; {testCase}");
+            }
+            catch (Exception)
+            {
+                if (testCase.ExpectedException != null)
+                    Console.WriteLine($"VALID TEST actual: {actual}; {testCase}");
+                else
+                    Console.WriteLine($"INVALID TEST actual: {actual}; {testCase}");
+            }
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Fibonaci(3));
-            Console.WriteLine(Fibonaci(4));
-            Console.WriteLine(Fibonaci(5));
-            Console.WriteLine(Fibonaci(6));
+            Test(new TestCase() { F = 3, Expected = 3 });
+            Test(new TestCase() { F = 4, Expected = 5 });
+            Test(new TestCase() { F = 5, Expected = 8 });
+            Test(new TestCase() { F = 6, Expected = 13 });
+
 
             Console.ReadKey();
         }

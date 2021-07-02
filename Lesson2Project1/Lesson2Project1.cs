1619789fc3e626b6a2fc0cee5df83785ddc96309
Collesss <sergey_lesikov@mail.ru>
2021-07-02 17:51:48 +0300

    update Project 1

---
+++ Lesson2Project1/Lesson2Project1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson2Project1
+{
+    class Lesson2Project1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lesson2Project1/LinkedList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2Project1
+{
+    class LinkedList : ILinkedList
+    {
+        private Node head;
+        private Node tail;
+
+        public LinkedList(){ }
+
+        public void AddNode(int value)
+        {
+            if (tail == null)
+            {
+                head = new Node() { Value = value };
+                tail = head;
+            }
+            else
+            {
+                tail.NextNode = new Node() { Value = value };
+                tail.NextNode.PrevNode = tail;
+                tail = tail.NextNode;
+            }
+        }
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            if (node == tail)
+            {
+                tail.NextNode = new Node() { Value = value };
+                tail.NextNode.PrevNode = tail;
+                tail = tail.NextNode;
+            }
+            else
+            {
+                node.NextNode.PrevNode = new Node() { Value = value };
+                node.NextNode = node.NextNode.PrevNode;
+            }
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            Node fn = head;
+            Node ln = tail;
+
+            if (fn == null)
+                return null;
+
+            do
+            {
+
+            }
+            while (fn != ln);
+
+            return null;
+        }
+
+        public int GetCount()
+        {
+
+        }
+
+        public void RemoveNode(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveNode(Node node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

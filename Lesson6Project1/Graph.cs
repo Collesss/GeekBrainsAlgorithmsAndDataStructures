701cac0dd0de7cd4e68e26355d458d01da830a71
Sergey Lesikov <sergey_lesikov@mail.ru>
2021-08-02 01:05:23 +0300

    update Project1

---
+++ Lesson6Project1/Graph.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson6Project1
+{
+    public static class Graph
+    {
+        public static int[] FindShortWayGraph(int[,] matrixWeights, int start, int end)
+        {
+            List<(int[] way, int len)> result = new List<(int[] way, int len)>();
+
+            Queue<(int vertex, int[] way, int len)> waves = new Queue<(int vertex, int[] way, int len)>();
+
+            
+            int maxLenWay = 0;
+
+            for (int i = 0; i < matrixWeights.GetLength(0); i++)
+                for (int j = 0; j < matrixWeights.GetLength(0); j++)
+                    if (matrixWeights[i, j] != int.MaxValue)
+                        maxLenWay++;
+
+            maxLenWay--;
+
+            waves.Enqueue((start, new int[] { start }, 0));
+
+            while (waves.Count > 0)
+            {
+                (int vertex, int[] way, int len) vertex = waves.Dequeue();
+
+                if (vertex.way[vertex.way.Length - 1] == end)
+                {
+                    result.Add((vertex.way, vertex.len));
+                    continue;
+                }
+
+                if (vertex.way.Length == maxLenWay)
+                    continue;
+
+                for (int i = 0; i < matrixWeights.GetLength(0); i++)
+                {
+                    if (vertex.vertex != i && !vertex.way.Contains(i) && matrixWeights[vertex.vertex, i] != int.MaxValue)
+                    {
+                        int[] newWay = new int[vertex.way.Length + 1];
+                        vertex.way.CopyTo(newWay, 0);
+                        newWay[newWay.Length - 1] = i;
+
+                        waves.Enqueue((i, newWay, vertex.len + matrixWeights[vertex.vertex, i]));
+                    }
+                }
+            }
+
+            return result.OrderBy(a => a.len).Select(el => el.way).FirstOrDefault() ?? new int[0];
+        }
+
+    }
+} 
--- Lesson6Project1/Lesson6Project1.cs
+++ Lesson6Project1/Lesson6Project1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Lesson6Project1
 {
@@ -7,6 +8,27 @@ namespace Lesson6Project1
         static void Main(string[] args)
         {
 
+            //A, B, C, D
+
+            int n = int.MaxValue;
+
+            int[,] matrixWeights = new int[,]
+                {
+                        //   A    B    C    D    E    F    G
+                    /*A*/{   n,   2,   n,  10,   n,   n,   n },
+                    /*B*/{   2,   n,  70,   3,   n,   n,   n },
+                    /*C*/{   n,  70,   n,   n,   2,   n,   n },
+                    /*D*/{  10,   3,   n,   n, 100,   n,   4 },
+                    /*E*/{   n,   n,   2, 100,   n,   3,   n },
+                    /*F*/{   n,   n,   n,   n,   3,   n,   1 },
+                    /*G*/{   n,   n,   n,   4,   n,   1,   n }
+                };
+
+
+            string[] graph = new string[] { "A", "B", "C", "D", "E", "F", "G"};
+
+            Console.WriteLine(string.Join("->", Graph.FindShortWayGraph(matrixWeights, 3, 2).Select(d => graph[d])));
+            Console.ReadKey();
         }
     }
 }

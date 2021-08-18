adaac674c97b7daed93b59da9cc0623479e9a598
Collesss <sergey_lesikov@mail.ru>
2021-08-18 17:50:13 +0300

    update Poject1

---
+++ Lesson7Project1/Dynamic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Lesson7Project1
+{
+    static class Dynamic
+    {
+        public static int NumberOptions(int n, int m)
+        {
+            int[,] matrix = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+                matrix[i, 0] = 1;
+
+            for (int i = 0; i < m; i++)
+                matrix[0, i] = 1;
+
+            for (int i = 1; i < n; i++)
+                for (int j = 1; j < m; j++)
+                    matrix[i, j] = matrix[i, j - 1] + matrix[i - 1, j];
+
+            return matrix[n - 1, m - 1];
+        }
+
+
+        //Ckn; n = N + M - 2; k = N - 1
+
+        public static BigInteger NumberOptionsFast(int n, int m)
+        {
+            int _n = n + m - 2;
+            int _k = n - 1;
+
+            BigInteger kFac = 1;
+            BigInteger nNotFullFac = 1;
+
+            int i;
+
+            for (i = 2; i <= _k; i++)
+                kFac *= i;
+
+            for (; i <= _n; i++)
+                nNotFullFac *= i;
+
+            return nNotFullFac / kFac;
+        }
+    }
+}
---
+++ Lesson7Project1/Lesson7Project1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson7Project1
+{
+    class Lesson7Project1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Dynamic.NumberOptions(7, 9));
+            Console.WriteLine(Dynamic.NumberOptionsFast(7, 9));
+
+            Console.ReadKey();
+        }
+    }
+}
